866b755e0bfb79ab04c5c20eda326db54382f316
rfichi <rdfs.ing@gmail.com>
2024-11-04 13:21:28 +0100

    Adding user input to specify the path of the files to be classified.

---
+++ README.md
@@ -0,0 +1,30 @@
+# File Classifier
+This project is use to manage and classify files by file extension.
+
+# Supported languages
+- English
+
+# Supported OS
+- Windows(with default path)
+- Linux(with default path)
+- Other(need to provide full path, no default path available)
+
+# Stack
+- python >= 3.7.9
+
+# Instructions to run the project
+- From virtual environment if it was previously created.
+```
+venv/bin/python3 classifier.py
+```
+- From global python interpreter
+```
+python3 classifier.py
+```
+
+
+# How to use the file classifier
+- The program will ask for the source path to manage the files and destination folder to store files by file extension.
+
+# To close the file classifier
+- The program will ask again to classify other directory files, if not enter introduce the word "close".
--- classifier.py
+++ classifier.py
@@ -3,6 +3,7 @@
 """
 import glob
 import os
+import platform
 import re
 import shutil
 import logging
@@ -51,7 +52,42 @@ def file_classifier(origin_dir, destiny_dir, mode="copy") -> Tuple[list, list]:
     return scanned_files, copied_files
 
 
+def get_default_path():
+    """
+    Get default path depending on the current OS
+    :return: _default - str
+    """
+    if "Windows" in platform.platform():
+        _default = f"C:{os.getenv('HOMEPATH')}/Downloads"  # Intended for Windows OS
+    elif "Linux" in platform.platform():
+        _default = os.getenv("HOME")  # Intended for Linux OS
+    else:
+        _default = ""
+    return _default
+
+
+def run():
+    """
+    Run program
+    Setting environment variables ORIGIN_DIRECTORY, DESTINY_DIRECTORY will overwrite any paths obtain as an input from
+    the user.
+    """
+    while True:
+        _default = get_default_path()
+        _origin = input(f"Introduce origin path to start file classification, default {_default}: ") or _default
+        _logger.info(f"Path to manage files {_origin}")
+        _destination = input("Add destination path to store files by file extension: ") or f"{_default}/classifier"
+        _logger.info(f"Path to destination folder {_destination}")
+        _mode = input("Select which mode to use:\n1) Copy\n2) Move\n")
+
+        origin_path = os.getenv("ORIGIN_DIRECTORY", _origin)
+        destiny_path = os.getenv("DESTINY_DIRECTORY", _destination)
+        file_classifier(origin_path, destiny_path, mode={"1": "copy", "2": "move"}.get(_mode, "1"))
+        running = input("Start new classifier process:\n1) Yes\n2) No\n")
+        if running != "1":
+            _logger.info("Exiting program")
+            break
+
+
 if __name__ == "__main__":
-    origin_path = os.getenv("ORIGIN_DIRECTORY")
-    destiny_path = os.getenv("DESTINY_DIRECTORY")
-    file_classifier(origin_path, destiny_path, mode="copy")
+    run()
